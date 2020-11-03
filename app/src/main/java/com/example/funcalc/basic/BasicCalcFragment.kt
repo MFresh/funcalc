e616df6db641b05a8e1c034f93487fb3ad65902e
MFresh <>
2020-11-03 16:15:23 +0100

    Basic calculator design

---
+++ app/src/main/java/com/example/funcalc/basic/BasicCalcFragment.kt
@@ -0,0 +1,53 @@
+package com.example.funcalc.basic
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.funcalc.R
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class BasicCalcFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        setHasOptionsMenu(true)
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_basic_calc, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_1).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        activity?.menuInflater?.inflate(R.menu.menu_basic, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_switch_to_scientific -> {
+
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
